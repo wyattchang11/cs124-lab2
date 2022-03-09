d7801ed2e598d92c1e98107b05c18de8cff6edcf
Wyatt CHang <changwyatt@gmail.com>
2022-03-09 14:11:33 -0800

    Co-authored-by: Chuck Rak <chuckrak@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -9,20 +9,46 @@ import { generateUniqueID } from 'web-vitals/dist/modules/lib/generateUniqueID';
 
 import '../src/style.css';
 
+import {useCollectionData} from "react-firebase-hooks/firestore";
+import {initializeApp} from "firebase/app";
+import {collection, deleteDoc, doc, getFirestore, updateDoc, query, setDoc, serverTimestamp} from "firebase/firestore";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyA56ajdAplN-Zf_wKrvBuhuxHvkXURp5lA",
+  authDomain: "cs124-lab3-4dca6.firebaseapp.com",
+  projectId: "cs124-lab3-4dca6",
+  storageBucket: "cs124-lab3-4dca6.appspot.com",
+  messagingSenderId: "677141143922",
+  appId: "1:677141143922:web:76e99db1d556bdc19f7deb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+const collectionName = "tasks";
 
 function App(props) {
-  const [tasks, setTask] = useState(props.initialData)
+  // const [tasks, setTask] = useState(props.initialData);
   const [showTaskEditor, setShowTaskEditor] = useState(false);
   const [taskToEdit, setTaskToEdit] = useState({});
-  function handleAdd(task, taskName) {
-    setTask([...task, { id: generateUniqueID(), task: taskName, completed: false }])
+
+  const q = query(collection(db, collectionName));
+  const [tasks, loading, error] = useCollectionData(q);
+  
+
+  function handleAdd(taskName) {
+    // setTask([...task, { id: generateUniqueID(), task: taskName, completed: false }])
+    const uniqueId = generateUniqueID();
+    setDoc(doc(db, collectionName, uniqueId),
+     {id: uniqueId, 
+      task:taskName, 
+      completed:false
+    });
   }
 
   function onItemChanged(taskID, field, value) {
-    setTask(tasks.map(
-      t => t.id === taskID ? {...t, [field]: value} : t
-    ))
+    updateDoc(doc(db, collectionName, taskID), {[field]:value});
   }
 
   function toggleTaskEditor(){
@@ -34,8 +60,21 @@ function App(props) {
   }
 
   function deleteCompletedTasks(task) {
-    setTask(tasks.filter(t => !t.completed));
+    // setTask(tasks.filter(t => !t.completed));
+    db.collection(collectionName)
+    .where('completed')
+    .forEach((doc) => deleteDoc(doc(db, collectionName, doc.id)))
   }
+  
+  if (loading) {
+    <p>Loading</p>
+  }
+
+  if (error) {
+    <p>ERROR</p>
+  }
+  
+  
 
   return (<div className="container">
     <Header/>
